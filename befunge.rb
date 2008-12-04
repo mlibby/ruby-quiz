65f9696a6ec0f41fab89b59ddd2239cb4aa3e1fb
Michael C. Libby <michael.c.libby@gmail.com>
2008-12-04 07:31:12 -0600

    updating to latest version from mcl_on_rails

--- befunge.rb
+++ befunge.rb
@@ -1,98 +1,295 @@
-#!/usr/bin/env ruby
+class Befunge
 
-class BefungeProgram
+  attr_accessor :direction, :no_skip, :stringmode
+  attr_reader :height, :width, :output, :pc_x, :pc_y, :program, :stack
 
-  HORIZ_SIZE = 80
-  MAX_X = HORIZ_SIZE - 1
-  VERT_SIZE = 25
-  MAX_Y = VERT_SIZE - 1
+  # commands are done this way to make it easy to push a Befunge instance
+  # into a Rails session. The procs don't like to be serialized if they 
+  # are in an instance or class variable, which would prevent needing to
+  # pass the instance in as a parameter... but this ain't so bad anyway
 
-  def initialize(program_lines)
-    raise "Error: Program exceeds 25 lines" if program_lines.length > VERT_SIZE
+  COMMANDS = {
+    ' ' => lambda{ }, # no op
+    '+' => lambda{|x| a,b = x.pop2i; x.push(b + a) }, # add
+    '-' => lambda{|x| a,b = x.pop2i; x.push(b - a) }, # subtract
+    '*' => lambda{|x| a,b = x.pop2i; x.push(b * a) }, # multiply
+    '/' => lambda{|x| a,b = x.pop2i; x.push(b / a) }, # divide
+    '%' => lambda{|x| a,b = x.pop2i; x.push(b % a) }, # modulo
+    '!' => lambda{|x| x.push(x.popi == 0 ? 1 : 0) }, # not
+    '`' => lambda{|x| a,b = x.pop2i; x.push(b > a ? 1 : 0) }, # greater
+    '>' => lambda{|x| x.direction = :right }, # right
+    '<' => lambda{|x| x.direction = :left }, # left
+    '^' => lambda{|x| x.direction = :up }, # up
+    'v' => lambda{|x| x.direction = :down }, # down
+    '?' => lambda{|x| x.direction = [:right, :left, :up, :down][rand(4)] }, # random
+    '_' => lambda{|x| x.direction = (x.popi == 0 ? :right : :left) }, # horiz if
+    '|' => lambda{|x| x.direction = (x.popi == 0 ? :down : :up) }, # vert if
+    '"' => lambda{|x| x.stringmode = (not x.stringmode) }, # stringmode
+    ':' => lambda{|x| a = x.pop; x.push(a); x.push(a) }, # dup
+    "\\" => lambda{|x| a,b = x.pop2; x.push(a); x.push(b) }, # swap
+    '$' => lambda{|x| x.pop }, # pop -> no op
+    '.' => lambda{|x| x.print_i(x.pop) }, # pop -> print as integer
+    ',' => lambda{|x| x.print_a(x.pop) }, # pop -> print as ascii
+    '#' => lambda{|x| x.move_pc }, # bridge (skip)
+    'g' => lambda{|x| a,b = x.pop2; x.push(x.get_cell(b,a)) }, # get
+    'p' => lambda{|x| a,b = x.pop2; v = x.pop; x.set_cell(v,b,a) }, # put
+    '&' => lambda{|x| x.input_i }, # input integer
+    '~' => lambda{|x| x.input_a }, # input ascii
+    '@' => lambda{|x| x.direction = :end } # end
+  }
 
-    @program = []
-    (0..MAX_Y).each do |y|
-      line = []
-      (0..MAX_X).each do |x|
-        line << ' '
-      end
-      @program << line
-    end
+  def initialize(program = nil, height = 25, width = 80)
+    @height = height
+    @width = width
+    #programs are stored as row, column (y, x)
+    @program =  Array.new(@height){|i| Array.new(@width, ' ') }
+    parse(program)
 
-    program_lines.each_index do |p|
-      line = program_lines[p].chomp.split(//)
-      raise "Error: Line #{p} exceeds 80 characters" if line.length > HORIZ_SIZE
-      line.each_index do |c|
-        @program[p][c] = line[c]
-      end
-    end
+    @output = ''
+    @stack = []
+    @pc_x = 0
+    @pc_y = 0
+    @direction = :right
+    @stringmode = false
+
+    @print = nil
+    @no_skip = false
   end
 
-  def print_program
-    print '#' * (HORIZ_SIZE + 2), "\n"
-    @program.each do |line|
-      print '#'
-      line.each do |char|
-        print char
-      end
-      print "#\n"
-    end
-    print '#' * (HORIZ_SIZE + 2), "\n"
+  def at_end?
+    @direction == :end
   end
 
-  def execute(peek = false)
-    pc = [0,0]
-    direction = :right
-    while true
-      puts "pc: #{pc.inspect}" if peek
-      puts "operation: #{@program[pc[0]][pc[1]]}" if peek
-      result = run_op(@program[pc[0]][pc[1]])
-      puts "result: #{result.nil? ? 'nil' : result}" if peek
-      if result.nil?
-        #nothing to do
-      elsif :exit == result
-        break
-      else
-        direction = result 
-      end
-      pc = move_pc(pc, direction)
-    end
+  def define_print(&block)
+    @print = block
   end
 
-  def run_op(operation)
-    result = nil
-    case operation
-    when '<': result = :left
-    when '>': result = :right
-    when '^': result = :up
-    when 'v': result = :down
-    end
-    return result
+  def get_cell(x, y)
+    @program[y.to_i][x.to_i]
   end
 
-  def move_pc(pc, direction)
-    case direction
+  def input_a
+    @stack << rand(255).chr
+  end
+
+  def input_i
+    @stack << rand(255)
+  end
+
+  def move_pc
+    case @direction
     when :right
-      pc[1] += 1
-      pc[1] = 0 if pc[1] > MAX_X
+      @pc_x = (@pc_x + 1 ) % @width
     when :left
-      pc[1] -= 1
-      pc[1] = MAX_X if pc[1] < 0
-    when :down
-      pc[0] += 1
-      pc[0] = 0 if pc[0] > MAX_Y
+      @pc_x = (@pc_x - 1 ) % @width
     when :up
-      pc[0] -= 1
-      pc[0] = MAX_Y if pc[0] < 0
+      @pc_y = (@pc_y - 1 ) % @height
+    when :down
+      @pc_y = (@pc_y + 1 ) % @height
+    when :end
+      # give it up, bro
+    else
+      raise "Invalid direction: #{@direction}"
     end
-    return pc
   end
 
+  def parse(program)
+    raise "Error: Program exceeds #{@height} lines (it has #{program.length})" if program.length >= @height
+    row_number = 0
+    program.each do |line|
+      column_number = 0
+      chars = line.chomp.split(//)
+      raise "Error: Line #{row_number + 1} exceeds #{@width} columns" if chars.length >= @width
+      chars.each do |char|
+        @program[row_number][column_number] = char
+        column_number += 1
+      end
+      row_number += 1
+    end
+  end
+
+  def pop
+    x = @stack.pop
+    x.nil? ? 0 : x
+  end
+
+  def popi
+    x = pop
+    i = (x == x.to_i) ? x : x[0]
+  end
+
+  def pop2
+    [pop, pop]
+  end
+
+  def pop2i
+    [popi, popi]
+  end
+
+  def push(a)
+    @stack << a
+  end
+
+  def print(s)
+    if @print.nil?
+      @output << s
+    else
+      @print.call(s)
+    end
+  end
+
+  def print_a(c)
+    if c.respond_to?(:chr)
+      print(c.chr)
+    else
+      print(c)
+    end
+  end
+
+  def print_i(c)
+    if c.respond_to?(:chr)
+      print(c.to_s)
+    else
+      print(c[0].to_s)
+    end
+  end
+
+  def set_cell(value, x, y)
+    if value.respond_to?(:chr)
+      @program[y.to_i][x.to_i] = value.chr
+    else
+      @program[y.to_i][x.to_i] = value
+    end
+  end
+
+  def skip_spaces
+    next_op = get_cell(@pc_x, @pc_y)
+    while next_op == " "
+      move_pc
+      next_op = get_cell(@pc_x, @pc_y)
+    end
+  end
+
+  def step
+    op = @program[@pc_y][@pc_x]
+    if @stringmode
+      if '"' == op
+        @stringmode = false
+      else
+        @stack << op
+      end
+    else
+      if COMMANDS.has_key?(op)
+        COMMANDS[op].call(self)
+      elsif op.match(/\d/)
+        @stack << op.to_i
+      else
+        # we could raise a syntax error?
+      end
+    end
+    move_pc
+    skip_spaces unless @stringmode || @no_skip
+    return op
+  end
+  
 end
 
+
+class BefungeConsoleRunner
+  
+  def initialize(program)
+    @befunge = Befunge.new(program)
+    @befunge.define_print{|c| print c }
+  end
+  
+  def run
+    @befunge.step until @befunge.at_end?
+  end
+  
+end
+
+
+class BefungeConsoleDebugger
+  
+  def initialize(program, debug_break)
+    @befunge = Befunge.new(program)
+    @debug_break = debug_break.nil? ? 16 : debug_break.to_i
+    @debug_count = 0
+    print_torus
+  end
+
+  def print_output
+    puts "Output: #{@befunge.output.inspect}"
+  end
+
+  def print_stack
+    puts "Stack: #{@befunge.stack.inspect}"
+  end
+
+  def print_torus
+    puts "\n", '#' * (@befunge.width + 2)
+    @befunge.program.each{|row| print '#', row.join, '#', "\n" }
+    puts '#' * (@befunge.width + 2), "\n" 
+  end
+
+  def print_registers
+    puts ["Program counter: (#{@befunge.pc_x}, #{@befunge.pc_y})",
+          "Direction: #{@befunge.direction}",
+          "Stringmode: #{@befunge.stringmode}"].join("; ")
+  end
+
+  def run
+    until @befunge.at_end?
+      @debug_count += 1
+      puts @befunge.step.inspect
+      
+      if @debug_count >= @debug_break
+        while true
+          print "[o]utput, [s]tack, [t]orus, [r]egisters, [q]uit, []continue (+ ENTER):"
+          
+          case $stdin.gets
+          when /o/i: print_output
+          when /s/i: print_stack
+          when /t/i: print_torus
+          when /r/i: print_registers
+          when /q/i: exit
+          when /^\n$/: break
+          else # invalid input, ask again
+          end
+        end
+        @debug_count = 0
+      end
+    end
+    puts @befunge.output
+  end
+  
+end
+
+
 if $0 == __FILE__
-  lines = File.readlines(ARGV[0])
-  bfp = BefungeProgram.new(lines)
-  bfp.print_program
-  bfp.execute(true)
+  mode = nil
+  case ARGV[0]
+  when /run/i : mode = :run
+  when /debug/i : mode = :debug
+  end
+  
+  # Valid mode and filename are required
+  if mode.nil? || ARGV.length < 2
+    puts "Usage: ruby befunge.rb <run|debug> <befunge.bf> <iterations>"
+    puts
+    puts "  run: run the program normally"
+    puts "debug: run the program in debug mode stopping every <iterations>"
+    puts "       to confirm continuation"
+    exit
+  end
+  
+  # Assume the user called with a valid filename
+  if mode == :run
+    bf = BefungeConsoleRunner.new(File.readlines(ARGV[1]))
+  else
+    bf = BefungeConsoleDebugger.new(File.readlines(ARGV[1]), ARGV[2])
+  end
+
+  bf.run
+  puts
 end
